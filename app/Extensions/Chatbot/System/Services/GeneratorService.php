9802ebaac6f0dd4cb31fa123b3fed025f18159c0
wajahat hussain <53147495+wajhiii@users.noreply.github.com>
2025-06-11 21:47:46 +0500

    Update GeneratorService.php

--- app/Extensions/Chatbot/System/Services/GeneratorService.php
+++ app/Extensions/Chatbot/System/Services/GeneratorService.php
@@ -46,7 +46,7 @@ class GeneratorService
         $driver
             ->input($generated)
             ->calculateCredit()
-            ->decreaseCredit();
+            ->decreaseCredit(1.00,$this->chatbot->user->id);
 
         return $generated;
     }
